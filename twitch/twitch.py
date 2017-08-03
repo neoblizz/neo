8aebc53103b9af762d167d8522a4a121032f7f73
neoblizz <osama94@gmail.com>
2017-08-03 16:26:43 -0700

    Fixed indentation.

--- twitch/twitch.py
+++ twitch/twitch.py
@@ -24,18 +24,16 @@ class Shoutout:
     async def shoutout(self, ctx, twitch : str):
         """Shoutout statements"""
         
-	username = twitch
-	bash = "./kraken.sh %s > kraken.log" % username
-
-	process = subprocess.Popen(bash.split(), stdout=subprocess.PIPE)
-	output, error = process.communicate()
-	data = json.loads(output.decode("utf-8"))
-
+        username = twitch
+        bash = "./kraken.sh %s > kraken.log" % username
+        process = subprocess.Popen(bash.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        data = json.loads(output.decode("utf-8"))
         auth = ctx.message.author
-	data = discord.Embed(colour=discord.Colour(0x942be2), url="https://twitch.tv/", description="A")
-	data.set_thumbnail(url="https://static-cdn.jtvnw.net/jtv_user_pictures/neoblizz-profile_image-8db895b58f67bd79-300x300.jpeg")
-	data.set_author(name="NeoBlizz", url="https://discordapp.com")
-	data.add_field(name="Aasdfasdf", value="Aasdfasdf")
+        data = discord.Embed(colour=discord.Colour(0x942be2), url="https://twitch.tv/", description="A")
+        data.set_thumbnail(url="https://static-cdn.jtvnw.net/jtv_user_pictures/neoblizz-profile_image-8db895b58f67bd79-300x300.jpeg")
+        data.set_author(name="NeoBlizz", url="https://discordapp.com")
+        data.add_field(name="Aasdfasdf", value="Aasdfasdf")
         
         await self.bot.say(embed=data)
 
